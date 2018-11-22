16c87083ae73f36af6aaee8b02429caac3b61fe1
Alex Xia <xia.alex.t@gmail.com>
2018-11-22 13:34:12 -0600

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoldDetectionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoldDetectionTest.java
@@ -79,7 +79,7 @@ public class GoldDetectionTest extends LinearOpMode {
 
         detector.enable();
 
-        //yes
+        //no
         move(350, 1500);
         turn(520,1500);
         sleep(1000);
