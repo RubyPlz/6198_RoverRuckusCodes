f3581ca7771766605d006d142a2ccd386ac2e7bd
ChaosDragon789 <ethan.m.chang@gmail.com>
2019-01-09 11:24:46 -0600

    first commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDepoStart.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDepoStart.java
@@ -1,13 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+import java.util.Locale;
+
 @Autonomous(name="DepoStart")
-public class AutoDepoStart extends LinearOpMode{
+public class IMUTest extends LinearOpMode{
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor frontLeftDrive = null;
@@ -15,12 +28,15 @@ public class AutoDepoStart extends LinearOpMode{
     private DcMotor backLeftDrive = null;
     private DcMotor backRightDrive = null;
 
-    private CRServo intake = null;
-    private CRServo upDownBoi = null;
 
     private double power = .2;
 
     private DcMotor actuator = null;
+
+    //IMU
+    BNO055IMU imu;
+    Orientation angles;
+    Acceleration gravity;
 
         @Override
         public void runOpMode() {
@@ -31,20 +47,43 @@ public class AutoDepoStart extends LinearOpMode{
             frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
             frontRightDrive.setDirection(DcMotor.Direction.REVERSE);backRightDrive.setDirection(DcMotor.Direction.REVERSE);
             frontLeftDrive.setPower(0);frontRightDrive.setPower(0);backLeftDrive.setPower(0);backRightDrive.setPower(0);
-            intake = hardwareMap.get(CRServo.class, "IntakeServo");
-            upDownBoi = hardwareMap.get(CRServo.class, "UpDownBoi");
-            intake.setDirection(CRServo.Direction.FORWARD);
+
             actuator = hardwareMap.get(DcMotor.class,"Actuator");
             actuator.setDirection(DcMotor.Direction.FORWARD);
             telemetry.addData("Robot", "Initialized");
             telemetry.update();
 
 
+            /**IMU
+             */
+            // Set up the parameters with which we will use our IMU. Note that integration
+            // algorithm here just reports accelerations to the logcat log; it doesn't actually
+            // provide positional information.
+            BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+            parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+            parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+            parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+            parameters.loggingEnabled      = true;
+            parameters.loggingTag          = "IMU";
+            parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
+            // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+            // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+            // and named "imu".
+            imu = hardwareMap.get(BNO055IMU.class, "imu");
+            imu.initialize(parameters);
+
+            // Set up our telemetry dashboard
+            composeTelemetry();
+            /**IMU
+             */
 
 
             // Wait for the game to start (driver presses PLAY)
             waitForStart();
+
+            imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
             runtime.reset();
 
 
@@ -62,19 +101,23 @@ public class AutoDepoStart extends LinearOpMode{
             backLeftDrive.setPower(power);
             backRightDrive.setPower(power);
 
+            actuator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            actuator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            actuator.setPower(1);
+            AutoDepoStart
 
             //move down
-            actuator.setPower(-1.0);
-            sleep(8000);
-            actuator.setPower(0.0);
-            turn(140,300);
+            /*good
+            actuator.setTargetPosition(-14030);
+            sleep(6000);
+            move(10,1000);
+            turnR();
+            */
 
-
-            /*main
+            /*
             move(1340,3000);
             turn(520,3000);
             move(2400,3000);
-            */
 
 
             //stop
@@ -83,7 +126,7 @@ public class AutoDepoStart extends LinearOpMode{
             backLeftDrive.setPower(0);
             backRightDrive.setPower(0);
             sleep(10000);
-
+            */
 
                 telemetry.addData("Status", "Run Time: " + runtime.toString());
                 telemetry.update();
@@ -91,19 +134,134 @@ public class AutoDepoStart extends LinearOpMode{
         }
 
     public void turn(int value,int sleepNum){
+        frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
         frontLeftDrive.setTargetPosition(-value);
         frontRightDrive.setTargetPosition(value);
         backLeftDrive.setTargetPosition(-value);
         backRightDrive.setTargetPosition(value);
         sleep(sleepNum);
+        stopMotors();
     }
-
     public void move(int value,int sleepNum){
+        frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
         frontLeftDrive.setTargetPosition(value);
         frontRightDrive.setTargetPosition(value);
         backLeftDrive.setTargetPosition(value);
         backRightDrive.setTargetPosition(value);
         sleep(sleepNum);
+        stopMotors();
 
+    }
+    public void turnL(){
+        turn(520,1100);
+    }
+    public void turnR(){
+        turn(-520,1100);
+    }
+    public void turnHL(){
+        turn(280,1000);
+    }
+    public void turnHR(){
+        turn(-280,1000);
+    }
+    public void stopMotors() {
+        frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    /**IMU
+     */
+
+    //----------------------------------------------------------------------------------------------
+    // Telemetry Configuration
+    //----------------------------------------------------------------------------------------------
+
+    void composeTelemetry() {
+
+        // At the beginning of each telemetry update, grab a bunch of data
+        // from the IMU that we will then display in separate lines.
+        telemetry.addAction(new Runnable() { @Override public void run()
+        {
+            // Acquiring the angles is relatively expensive; we don't want
+            // to do that in each of the three items that need that info, as that's
+            // three times the necessary expense.
+            angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            gravity  = imu.getGravity();
+        }
+        });
+
+        telemetry.addLine()
+                .addData("status", new Func<String>() {
+                    @Override public String value() {
+                        return imu.getSystemStatus().toShortString();
+                    }
+                })
+                .addData("calib", new Func<String>() {
+                    @Override public String value() {
+                        return imu.getCalibrationStatus().toString();
+                    }
+                });
+
+        telemetry.addLine()
+                .addData("heading", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.firstAngle);
+                    }
+                })
+                .addData("roll", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.secondAngle);
+                    }
+                })
+                .addData("pitch", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.thirdAngle);
+                    }
+                });
+
+        telemetry.addLine()
+                .addData("grvty", new Func<String>() {
+                    @Override public String value() {
+                        return gravity.toString();
+                    }
+                })
+                .addData("mag", new Func<String>() {
+                    @Override public String value() {
+                        return String.format(Locale.getDefault(), "%.3f",
+                                Math.sqrt(gravity.xAccel*gravity.xAccel
+                                        + gravity.yAccel*gravity.yAccel
+                                        + gravity.zAccel*gravity.zAccel));
+                    }
+                });
+    }
+
+    //----------------------------------------------------------------------------------------------
+    // Formatting
+    //----------------------------------------------------------------------------------------------
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
     }
 }
